dcf8920fa9b0ab68b0bc98fbd1edc17d8c41c6d6
Paulo Brusch <contato.paulobrusch@gmail.com>
2024-02-27 21:42:05 -0300

    Getting a user by id

--- src/main/java/com/paulobrusch/workshopmongo/services/UserService.java
+++ src/main/java/com/paulobrusch/workshopmongo/services/UserService.java
@@ -2,19 +2,26 @@ package com.paulobrusch.workshopmongo.services;
 
 import com.paulobrusch.workshopmongo.domain.User;
 import com.paulobrusch.workshopmongo.repository.UserRepository;
+import com.paulobrusch.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository repository;
 
     public List<User> findAll() {
-        return repo.findAll();
+        return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
 
